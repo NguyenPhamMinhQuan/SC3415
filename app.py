39c2712b2f3fd639de16a80302c9434b9113ec05
NguyenPhamMinhQuan <88914475+NguyenPhamMinhQuan@users.noreply.github.com>
2026-01-19 08:28:24 +0000

    Complete the outline for DBSPrediction

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
+import joblib
+import sklearn
+
+#Import pre-trained model
+model = joblib.load("./model/dbs.jl")
+
+
 @app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/main", methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/dbs", methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
 
 if __name__ == "__main__":
     app.run()
